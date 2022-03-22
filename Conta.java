76a9e366f834bc598a339d85e0ce74efcd74366c
Camila Lacerda Grandini <83710007+camilagrandinii@users.noreply.github.com>
2022-03-22 20:42:50 -0300

    Added files via upload

---
+++ Conta.java
@@ -0,0 +1,70 @@
+/**
+ * TP I - Algoritmos e Estruturas de Dados III
+ * @author - Camila Lacerda Grandini & Joana Woldaysnky
+ * 2022 - 3o. Semestre
+ */
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+public class Conta {
+  protected int idConta;
+  protected String nomePessoa;
+  protected String cpf;
+  protected String cidade;
+  protected int transferenciasRealizadas;
+  protected float saldoConta;
+  protected boolean lapide;
+
+  public Conta(int id, String nome, String cpf, String cidade) {
+    this.idConta = id;
+    this.nomePessoa = nome;
+    this.cpf = cpf;
+    this.cidade = cidade;
+    this.transferenciasRealizadas = 0;
+    this.saldoConta = 0;
+    this.lapide = false;
+  }
+
+  public Conta() {
+    this.idConta = -1;
+    this.nomePessoa = "";
+    this.cpf = "";
+    this.cidade = "";
+    this.transferenciasRealizadas = 0;
+    this.saldoConta = 0;
+    this.lapide=false;
+  }
+
+  public String toString() {
+    return "\nID....: " + this.idConta + "\nNome Pessoa.: " + this.nomePessoa + "\nCPF.: " + this.cpf + "\nCidade.: " + this.cidade + "\nTransferências Realizadas.: " + this.transferenciasRealizadas + "\nSaldo.: " + this.saldoConta;
+  }
+
+  public byte[] toByteArray() throws IOException {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(baos);
+    dos.writeBoolean(lapide);
+    dos.writeInt(idConta);
+    dos.writeUTF(nomePessoa);
+    dos.writeUTF(cpf);
+    dos.writeUTF(cidade);
+    dos.writeInt(transferenciasRealizadas);
+    dos.writeFloat(saldoConta);
+    return baos.toByteArray();
+  }
+
+  public void fromByteArray(byte[] ba) throws IOException {
+    ByteArrayInputStream bais = new ByteArrayInputStream(ba);
+    DataInputStream dis = new DataInputStream(bais);
+    idConta = dis.readInt();
+    nomePessoa = dis.readUTF();
+    cpf = dis.readUTF();
+    cidade = dis.readUTF();
+    transferenciasRealizadas = dis.readInt();
+    saldoConta = dis.readFloat();
+  }
+
+}
---
+++ Main.java
@@ -0,0 +1,122 @@
+/**
+ * TP I - Algoritmos e Estruturas de Dados III
+ * @author - Camila Lacerda Grandini & Joana Woldaysnky
+ * 2022 - 3o. Semestre
+ */
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+  public static void criaConta(int id){
+    Scanner sc = new Scanner(System.in);
+      ArrayList<Conta> alunosWrite = new ArrayList<Conta>();
+      System.out.println("Escreva o seu nome: ");
+      String nome = sc.next();
+      System.out.println("Escreva o seu cpf (sem tracos e/ou pontos): ");
+      String cpf = sc.next();
+      System.out.println("Escreva a sua cidade: ");
+      String cidade = sc.next();
+
+      Conta aluno = new Conta(id, nome, cpf, cidade);
+      alunosWrite.add(aluno);
+    sc.close();
+  }
+  public static void realizaTransferencia(){
+    Scanner sc = new Scanner(System.in);
+      System.out.println("Qual será a conta debitada? (cpf sem traços e/ou pontos) ");
+      String cpf_debito = sc.next();
+      System.out.println("Qual será a conta creditada? (cpf sem traços e/ou pontos) ");
+      String cpf_credito = sc.next();
+      System.out.println("Escreva o valor da transferencia: ");
+      float valor_transf = sc.nextFloat();
+    sc.close();
+  }
+  public static void leRegistro(int id) throws Exception{
+    RandomAccessFile arq; //ja traz os métodos do dataOutputStream ou inputStream
+    try{
+      arq = new RandomAccessFile("dados/conta.db", "rw");
+    
+      byte[] ba;
+      int tam;
+
+      arq.seek(0);
+      while (arq.getFilePointer()<arq.length()){ 
+        Conta aRead = new Conta();
+        tam = arq.readInt();
+        ba = new byte[tam];
+        arq.read(ba);
+        aRead.fromByteArray(ba);
+        
+        if (id==aRead.idConta)
+        aRead.toString();
+      }
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  public static void printMenu(){
+    System.out.println("MENU: \n1 - Criar uma Conta Bancária \n2 - Realizar uma Transferência \n3 - Ler um Registro \n4 - Atualizar um Registro \n5 - Deletar um Registro\n6 - Sair ");
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    printMenu();
+    ArrayList<Conta> alunosRead = new ArrayList<Conta>();
+    int op=sc.nextInt();
+    int id=0;
+    do{
+        switch (op){
+          case 1:
+          criaConta(id);
+          id++;
+          break;
+
+          case 2:
+          realizaTransferencia();
+          break;
+
+          case 3:
+          System.out.println("Qual o registro desejado para leitura? ");
+          try{ leRegistro(id); 
+          }catch(Exception e){
+            e.printStackTrace();
+          }
+          break;
+
+          case 4:
+          break;
+
+          case 5:
+          break;
+
+          case 6:
+          System.out.println("Saindo do programa...");
+          break;
+        }
+        printMenu();
+        op=sc.nextInt(); 
+    }while(op==0);
+        sc.close();
+
+    RandomAccessFile arq; //ja traz os métodos do dataOutputStream ou inputStream
+    byte[] ba;
+    int tam;
+
+    /*
+        arq = new RandomAccessFile("dados/conta.db", "rw");
+        for (int i=0; i<alunosWrite.size(); i++){
+            ba = alunosWrite.get(i).toByteArray();
+            arq.writeInt(ba.length);
+            arq.write(ba);
+        }
+    */
+
+    for (int i=0; i<alunosRead.size(); i++){
+      System.out.println(alunosRead.get(i));
+    }
+    
+  }
+}
