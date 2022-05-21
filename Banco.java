c59f1b4592441fe8dfdcd3f6374a144eedb520c4
Camila Lacerda Grandini <cacagrandini@gmail.com>
2022-05-21 12:48:39 -0300

    changed the sequencial search to the binary search on all methods

--- Banco.java
+++ Banco.java
@@ -10,7 +10,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Banco{
-    RandomAccessFile arq;
+    RandomAccessFile arq, arqIndice;
     Scanner sc;
 
     /*
@@ -18,6 +18,8 @@ public class Banco{
     */
     Banco(){
         try{ arq = new RandomAccessFile("dados/banco.db", "rw");
+        }catch(FileNotFoundException e){ System.out.println("Erro: Arquivo nao encontrado!");}   
+        try{ arqIndice = new RandomAccessFile("dados/indicesDados.db", "rw");
         }catch(FileNotFoundException e){ System.out.println("Erro: Arquivo nao encontrado!");}   
         sc = new Scanner(System.in);
     }
@@ -27,8 +29,35 @@ public class Banco{
     */
     public void closeFile() throws Exception{
         arq.close();
+        arqIndice.close();
     }
+    /*
+    Método que testa se o CPF da conta criada já existe
+    SE SIM, não deixa criar a conta
+    */
+    public boolean testeCpfJaExistente(String cpf) throws Exception{
+        boolean teste_cpf=false;
+        int tam;
+        byte[] ba;
+        try{
+            arq.seek(4); //pula o maior ID que esta gravado no arquivo
 
+            while (arq.getFilePointer()<arq.length()){ 
+            Conta cRead = new Conta();
+            tam = arq.readInt();
+            ba = new byte[tam];
+            arq.read(ba);
+            cRead.fromByteArray(ba);
+
+                if (cpf.equals(cRead.cpf)){
+                teste_cpf=true;
+                }
+            }
+        } catch(IOException e){
+            System.out.println("Erro: Nao foi possível ler o arquivo!");
+        }
+        return teste_cpf;
+    }
     /*
     Método que testa se o ID da conta criada é maior que o maior ID salvo
     SE SIM, salva o ID da conta criada no lugar do outro
@@ -49,13 +78,12 @@ public class Banco{
     em uma registro contendo: lápide, id, nome, cpf, cidade, transf realizadas e saldo
     */
     public void criaConta(int id) throws Exception{
-        byte[] ba;
+        byte[] ba, baIndice;
         boolean teste = false;
         String nome="", cpf="", cidade="";
 
         while(teste==false){
             System.out.println("Escreva o seu nome: ");
-            sc.nextLine();
             nome = sc.nextLine();
             if(nome.length()<100)
             teste=true;
@@ -70,6 +98,10 @@ public class Banco{
             if(cpf.length()==11)
             teste=true;
             else System.out.println("Digite um CPF valido!");
+        }
+        if (testeCpfJaExistente(cpf)==true)
+        {
+            throw new Exception("CPF ja cadastrado!");
         }
 
         teste=false;
@@ -97,8 +129,16 @@ public class Banco{
             }
 
             arq.seek(arq.length());
+
+            long pos1 = arq.getFilePointer(); //pega o endereço no qual está localizado o proximo registro que será escrito
+            
+            baIndice=conta.toByteArrayIndice(pos1); //cria um array de bytes com as informações: id, cpf e endereço de localização
+
             arq.writeInt(ba.length);
             arq.write(ba);
+
+            arqIndice.seek(arqIndice.length());
+            arqIndice.write(baIndice); //escreve o array de bytes no arquivo de indice
 
         } catch(IOException e){
             System.out.println("Erro: Nao foi possível ler o arquivo!");
@@ -142,50 +182,48 @@ public class Banco{
     */
     public void realizaTransf(String cpf_debito, String cpf_credito, float valor) throws Exception{
         byte[] ba;
-        int tam;    
+        int tam;   
+        long end_credito, end_debito;
             try{
-                arq.seek(4); //pula o maior ID que esta gravado no arquivo
-
-                while (arq.getFilePointer()<arq.length()){ 
+                end_debito = pesquisa_binariaString(cpf_debito);
                 Conta cRead = new Conta();
 
-                long pos1 = arq.getFilePointer();
+                if(end_debito!=-1){
+                arq.seek(end_debito);
                 tam = arq.readInt();
                 ba = new byte[tam];
                 arq.read(ba);
                 cRead.fromByteArray(ba);
-                System.out.println("cpf lido: "+cRead.cpf);
-                    if (cpf_debito.equals(cRead.cpf)){
+
                     cRead.saldoConta-=valor;
                     cRead.transferenciasRealizadas++;
 
                     ba = cRead.toByteArray();
-                    arq.seek(pos1);
+                    arq.seek(end_debito);
                     arq.writeInt(ba.length);
                     arq.write(ba);
-                    }
                 }
+                else System.out.println("Erro: cpf informado para debito nao foi encontrado");
 
-                arq.seek(4); //pula o maior ID que esta gravado no arquivo
-                while (arq.getFilePointer()<arq.length()){ 
-                    Conta cRead = new Conta();
-    
-                    long pos1 = arq.getFilePointer();
-                    tam = arq.readInt();
-                    ba = new byte[tam];
-                    arq.read(ba);
-                    cRead.fromByteArray(ba);
-                    
-                        if (cpf_credito.equals(cRead.cpf)){
-                        cRead.saldoConta+=valor;
-                        cRead.transferenciasRealizadas++;
-    
-                        ba = cRead.toByteArray();
-                        arq.seek(pos1);
-                        arq.writeInt(ba.length);
-                        arq.write(ba);
-                        }
+                end_credito = pesquisa_binariaString(cpf_credito);
+
+                if(end_credito!=-1){
+                arq.seek(end_credito);
+                tam = arq.readInt();
+                ba = new byte[tam];
+                arq.read(ba);
+                cRead.fromByteArray(ba);
+
+                    cRead.saldoConta+=valor;
+                    cRead.transferenciasRealizadas++;
+
+                    ba = cRead.toByteArray();
+                    arq.seek(end_credito);
+                    arq.writeInt(ba.length);
+                    arq.write(ba);
                 }
+                if(end_credito==-1)
+                System.out.println("Erro: cpf informado para credito nao foi encontrado");
                 
             } catch(IOException e){
                 System.out.println("Erro: Nao foi possível ler o arquivo!");
@@ -197,26 +235,31 @@ public class Banco{
     */
     public void leRegistro() throws Exception{
         System.out.println("Qual o registro desejado para leitura? ");
-        int id = sc.nextInt();
+        int id = sc.nextInt(), tam;
         byte[] ba;
-        int tam;
+        long end_leitura;
 
         try{
-
-            arq.seek(4); //pula o maior ID que esta gravado no arquivo
-            while (arq.getFilePointer()<arq.length()){ 
+            end_leitura=pesquisa_binaria(id);
+            if(end_leitura!=-1){
                 Conta cRead = new Conta();
+                arq.seek(end_leitura);
                 tam = arq.readInt();
                 ba = new byte[tam];
                 arq.read(ba);
                 cRead.fromByteArray(ba);
                 
+                System.out.println("cRead lapide = "+cRead.lapide+"id conta: "+cRead.idConta);
+
                 if (cRead.lapide==false && id==cRead.idConta){
                     System.out.println(cRead.toString()+"\n");
                 }
-                else
-                System.out.println("Erro: ID nao encontrado!");
+                else if(cRead.lapide==true && id==cRead.idConta){
+                    System.out.println("O registro desejado foi excluido!");
+                }
             }
+            else
+                System.out.println("Erro: ID nao encontrado!");
             
         } catch(IOException e){
             System.out.println("Erro: Nao foi possível ler o arquivo!");
@@ -229,32 +272,31 @@ public class Banco{
     public void deletaRegistro() throws Exception{
         System.out.println("Qual registro voce deseja deletar? ");
         int id = sc.nextInt();
+        byte[] ba;
+        int tam;
+        long end_deleta;
     
         try{
-            byte[] ba;
-            int tam;
-            arq.seek(4); //pula o maior ID que esta gravado no arquivo
-
-            while (arq.getFilePointer()<arq.length()){ 
+            end_deleta = pesquisa_binaria(id);
+            if(end_deleta!=-1){
             Conta cRead = new Conta();
-            long pos1 = arq.getFilePointer();
+            arq.seek(end_deleta);
             tam = arq.readInt();
             ba = new byte[tam];
             arq.read(ba);
             cRead.fromByteArray(ba);
             
-                if (id==cRead.idConta){
-                cRead.lapide=true;
+            cRead.lapide=true;
 
-                ba = cRead.toByteArray();
-                    arq.seek(pos1);
-                    arq.writeInt(ba.length);
-                    arq.write(ba);
-                }
-                else{
-                    System.out.println("Erro: ID nao encontrado!");
-                }
+            ba = cRead.toByteArray();
+                arq.seek(end_deleta);
+                arq.writeInt(ba.length);
+                arq.write(ba);
+                
+            atualizaEnderecoIndice(cRead.idConta, -1);
             }
+            else System.out.println("Erro: ID nao encontrado!");
+
         } catch(IOException e){
             System.out.println("Erro: Nao foi possível ler o arquivo!");
         }
@@ -266,24 +308,21 @@ public class Banco{
     public void atualizaRegistro() throws Exception{
         System.out.println("Qual registro voce deseja atualizar? ");
         int id = sc.nextInt();
+        byte[] ba;
+        int tam;
+        long end_atualiza;
         boolean teste=false;
     
         try{
-            byte[] ba;
-                int tam;
-                arq.seek(4); //pula o maior ID que esta gravado no arquivo
-
-                while (arq.getFilePointer()<arq.length()){ 
+                end_atualiza = pesquisa_binaria(id);
+                if(end_atualiza!=-1){
                 Conta cRead = new Conta();
-
+                arq.seek(end_atualiza);
                 tam = arq.readInt();
                 ba = new byte[tam];
                 arq.read(ba);
                 cRead.fromByteArray(ba);
-                
-                    if (id==cRead.idConta){
-                    long pos1 = arq.getFilePointer();
-
+    
                         while(teste==false){
                             System.out.println("Escreva o seu nome: ");
                             sc.nextLine();
@@ -313,29 +352,134 @@ public class Banco{
                             teste=true;
                             else System.out.println("Nome da cidade invalido!");
                         }
-
+                        
                         ba = cRead.toByteArray();
+
                         if(tam==ba.length){
-                        arq.seek(pos1);
+                        arq.seek(end_atualiza);
                         arq.writeInt(ba.length);
                         arq.write(ba);
                         }
                         else{
-                        arq.seek(pos1+4); //pula o tamanho da instancia que esta gravada no arquivo
+                        arq.seek(end_atualiza+4); //pula o tamanho da instancia que esta gravada no arquivo
                         arq.writeBoolean(true);
 
-                        ba = cRead.toByteArray();
                         arq.seek(arq.length());
+                        long pt = arq.getFilePointer();
+
                         arq.writeInt(ba.length);
                         arq.write(ba);
+
+                        atualizaEnderecoIndice(cRead.idConta, pt);
                         }
-                    }
-                    else{
-                        System.out.println("Erro: O ID informado nao foi encontrado");
-                    }
                 }
+                else System.out.println("Erro: O ID informado nao foi encontrado");
         } catch(IOException e){
             System.out.println("Erro: Nao foi possível ler o arquivo!");
         }
+    }
+    /*
+    Método que procura o ultimo de id salvo no arquivo para determinar quantos registros estão salvos nele
+    */
+    public int numContas(){
+        int numContas=0;
+        try{
+            arq.seek(0);
+            numContas = arq.readInt();
+        }catch(Exception e){}
+        return numContas;
+    }
+    /*
+    Método que procura le todos os dados presentes no arquivo de indices
+    */
+    public Conta[] lerTodosDados(){
+        int cont=0, tam;
+        byte[] ba;
+        tam = numContas();
+        tam++;
+        Conta[] cRead = new Conta[tam];
+        try{
+            arqIndice.seek(0);
+            while (arqIndice.getFilePointer()<arqIndice.length()){ 
+                cRead[cont] = new Conta();
+                ba = new byte[25];
+                arqIndice.read(ba);
+                cRead[cont].fromByteArrayIndice(ba);
+                cont++;
+            }
+        }catch(Exception e){}
+        return cRead;
+    }
+    /*
+    Método que realiza uma pesquisa binaria nos dados do arquivo de indice
+    Tomando como base para a pesquisa os dados salvos no vetor de contas
+    */
+    public long pesquisa_binaria(int pesquisa_id){
+        int cont = numContas();
+        cont++; // devemos somar 1 pra criar o vetor com posições suficientes para caber todos os registros do arq indice
+        Conta[] contas = new Conta[cont];
+        contas = lerTodosDados();
+
+        int beggining = 0, end = cont-1;
+            while (beggining <= end) {
+                int middle = beggining + (end - beggining) / 2;
+                
+                if (pesquisa_id==contas[middle].idConta) //achou 
+                    return contas[middle].end; // retorna endereço da pesquisa no arquivo de dados (se a pesquisa estiver presente nele)
+                
+                else if (pesquisa_id>contas[middle].idConta) // Se a string é maior, ignora a metade da esquerda
+                    beggining = middle + 1;
+
+                else // Se a string é menor é menor, ignora a metade da direita
+                    end = middle - 1;
+            }
+        return -1; // else return -1 
+    }
+    /*
+    Método que atualiza no arquivo de índice o endereço do registro no arquivo 
+    de dados
+    */
+    public void atualizaEnderecoIndice(int pesquisa_id, long ponteiro){
+        Conta cRead;
+        byte[] ba;
+        long pt;
+        try{
+            arqIndice.seek(0);
+            while(arqIndice.getFilePointer()<arqIndice.length()){
+                pt = arqIndice.getFilePointer();
+                cRead = new Conta();
+                ba = new byte[25];
+                arqIndice.read(ba);
+                cRead.fromByteArrayIndice(ba);
+                if(cRead.idConta==pesquisa_id){
+                    arqIndice.seek(pt);
+                    arqIndice.write(cRead.toByteArrayIndice(ponteiro));
+                }
+            }
+        }catch(Exception e){}
+    }
+    /*
+    Método que realiza uma pesquisa binaria nos dados do arquivo de indice
+    Tomando como base para a pesquisa os dados salvos no vetor de contas
+    */
+    public long pesquisa_binariaString(String pesquisa_string){
+        int cont = numContas();
+        Conta[] contas = new Conta[cont];
+        contas = lerTodosDados();
+		// Returns index da pesquisa no array (se a pesquisa estiver presente nela)
+        int beggining = 0, end = cont-1;
+            while (beggining <= end) {
+                int middle = beggining + (end - beggining) / 2;
+
+                if (pesquisa_string.compareTo(contas[middle].cpf)==0) //achou 
+                    return contas[middle].end;
+                
+                if (pesquisa_string.compareTo(contas[middle].cpf)>0) // Se a string é maior, ignore left half
+                    beggining = middle + 1;
+
+                else // Se a string é menor é menor, ignore right half
+                    end = middle - 1;
+            }
+        return -1; // else return -1 
     }
 }
--- Conta.java
+++ Conta.java
@@ -17,6 +17,7 @@ public class Conta {
   protected int transferenciasRealizadas;
   protected float saldoConta;
   protected boolean lapide;
+  protected long end;
 
       /*
     Método construtor da classe conta
@@ -42,6 +43,7 @@ public class Conta {
     this.transferenciasRealizadas = 0;
     this.saldoConta = 0;
     this.lapide=false;
+    this.end=0x0;
   }
 
   public String toString() {
@@ -65,6 +67,19 @@ public class Conta {
   }
 
     /*
+    Método para transformar os dados de uma instância da classe conta em um array de bytes (mas com 
+    menos informações)
+    */
+    public byte[] toByteArrayIndice(long end_lido) throws IOException {
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      DataOutputStream dos = new DataOutputStream(baos);
+      dos.writeInt(idConta);
+      dos.writeUTF(cpf);
+      dos.writeLong(end_lido);
+      return baos.toByteArray();
+    }
+
+    /*
     Método para transformar o array de bytes em uma classe com seus atributos que descrevam os dados de uma instância da classe conta
     */
   public void fromByteArray(byte[] ba) throws IOException {
@@ -78,4 +93,15 @@ public class Conta {
     transferenciasRealizadas = dis.readInt();
     saldoConta = dis.readFloat();
   }
+      /*
+    Método para transformar o array de bytes em uma classe com alguns atributos que descrevam os dados 
+    de uma instância da classe conta
+    */
+    public void fromByteArrayIndice(byte[] ba) throws IOException {
+      ByteArrayInputStream bais = new ByteArrayInputStream(ba);
+      DataInputStream dis = new DataInputStream(bais);
+      idConta = dis.readInt();
+      cpf = dis.readUTF();
+      end = dis.readLong();
+    }
 }
